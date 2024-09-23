9949b74335a40f7a1606b45e1e54d659f1efb2b3
Yoan Commeau <yoan.commeau@ynov.com>
2024-09-23 09:10:32 +0200

    Début arborescence et listes de mots

---
+++ src/function/func.go
@@ -0,0 +1,11 @@
+package Hangman
+
+import "fmt"
+
+func Menu() {
+	fmt.Println("Welcome to Hangman!")
+	fmt.Println("Choose an option:")
+	fmt.Println("1. Play a new game")
+	fmt.Println("2. Exit")
+	fmt.Print("Enter your choice: ")
+}
