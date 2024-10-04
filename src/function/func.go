f5dc8a531946b52bc5fdb84d6c0a1ecd1eca80b1
Yoan Commeau <yoan.commeau@ynov.com>
2024-10-04 20:53:54 +0200

    Hangman fini, j'ai tout fait d'un coup j'ai pas pensée à commit sorry

--- src/function/func.go
+++ src/function/func.go
@@ -1,17 +1,23 @@
 package Hangman
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"math/rand"
 	"os"
 	"os/exec"
 	"runtime"
+	"slices"
+	"strings"
 )
 
-var Word string       //mot a deviner
-var TabUnder []string //tableau d'underscore
-var Win bool          //verif si on a win (si y'a plus d'underscore)
-var Guessed []string  //liste des lettres/mots déjà rentrer
-var Graph int         //compteur de point pour le graph
+var Word string                //mot a deviner
+var TabUnder []string          //tableau d'underscore
+var win bool                   //verif si on a win (si y'a plus d'underscore)
+var LetterGuessedList []string //liste des lettres déjà rentrer
+var WordGuessedList []string   //liste des mots déjà rentrer
+var Graph int                  //compteur de point pour le graph
 
 // Start Menu
 func Menu() {
@@ -27,6 +33,7 @@ func Menu() {
 	case "1":
 		NewGame()
 	case "2":
+		ClearTerminal()
 		fmt.Println("Exiting...")
 		return
 	default:
@@ -37,12 +44,13 @@ func Menu() {
 
 // NewGame Menu
 func NewGame() {
+	ClearTerminal()
 	Word = ""
 	TabUnder = []string{}
-	Win = false
-	Guessed = []string{}
+	win = false
+	LetterGuessedList = []string{}
+	WordGuessedList = []string{}
 	Graph = 0
-	ClearTerminal()
 	var input string
 	fmt.Println("Choisissez une difficulté...")
 	fmt.Println("1. Facile")
@@ -51,10 +59,11 @@ func NewGame() {
 	fmt.Scan(&input)
 	switch input {
 	case "1":
-		Underscore("Après-midi")
-		Display()
-	case "3":
-		//hangmanGame("hard")
+		ClearTerminal()
+		ShowTextFromFile("src/wordList/wordList_Easy.txt")
+	case "2":
+		ClearTerminal()
+		ShowTextFromFile("src/wordList/wordList_Hard.txt")
 	default:
 		fmt.Println("Pas compris")
 		NewGame()
@@ -70,6 +79,8 @@ func ClearTerminal() {
 	}
 }
 
+// Tableau d'underscore correspondant au mot à deviner
+// Params : word = mot choisis dans la liste de mot
 func Underscore(word string) {
 	Word = word
 	for _, i := range word {
@@ -81,15 +92,22 @@ func Underscore(word string) {
 	}
 }
 
+// Affichage de base
 func Display() {
+	DisplayHangman()
 	fmt.Print("Voici le mot à deviner : ")
 	for _, i := range TabUnder {
 		fmt.Print(i, " ")
 	}
 	fmt.Println("")
+	fmt.Println("Lettres déjà tenté : ", LetterGuessedList)
+	fmt.Println("")
+	fmt.Println("Mots déjà tenté : ", WordGuessedList)
+	fmt.Println("")
 	Guess()
 }
 
+// Menu pour taper votre lettre/mot
 func Guess() {
 	var input string
 	fmt.Print("Entrez une lettre ou un mot: ")
@@ -101,105 +119,248 @@ func Guess() {
 	}
 }
 
+// Check si le guess est dans le mot
 func GuessLetter(letter string) {
-	if !IsInGuessed(letter) {
-		Guessed = append(Guessed, letter)
-		for _, v := range Word {
-			if string(v) == letter {
-				ChangeTableau(letter)
+	if !IsLetterInGuessed(letter) {
+		LetterGuessedList = append(LetterGuessedList, strings.ToLower(letter))
+		if IsInWord(letter) {
+			ChangeTableau(letter)
+			if !CheckWin() {
+				ClearTerminal()
 				Display()
-			} else {
-				fmt.Println("Mauvaise réponse !")
-				Graph += 1
-				CheckLoose()
 			}
-		}
-	} else {
-		fmt.Println("Vous avez déjà essayé cette lettre!")
-		Display()
-	}
-}
-
-func GuessWord(word string) {
-	if !IsInGuessed(word) {
-		Guessed = append(Guessed, word)
-		if word == Word {
-			Win = true
-			CheckWin()
 		} else {
-			fmt.Println("Mauvaise réponse!")
-			Graph += 2
+			ClearTerminal()
+			fmt.Println("Mauvaise réponse !")
+			fmt.Println("")
+			fmt.Println("")
+			Graph += 1
 			CheckLoose()
 		}
 	} else {
-		fmt.Println("Vous avez déjà essayé ce mot!")
+		ClearTerminal()
+		fmt.Println("Vous avez déjà essayé cette lettre!")
+		fmt.Println("")
+		fmt.Println("")
 		Display()
 	}
 }
 
-func ChangeTableau(guess string) {
-	for i, v := range Word {
-		if string(v) == guess {
-			TabUnder[i] = guess
-		}
-	}
-}
-
-func CheckWin() {
-	ClearTerminal()
-	if Win {
-		fmt.Println("Vous avez gagné!")
-		fmt.Println("Le mot était :", Word)
-		fmt.Println("Refaire une partie ?")
-		fmt.Println("1: Oui")
-		fmt.Println("2: Non")
-		var input string
-		fmt.Print("Votre choix: ")
-		fmt.Scan(&input)
-		switch input {
-		case "1":
-			NewGame()
-		case "2":
-			fmt.Println("Exiting...")
-			os.Exit(0)
-		default:
-			fmt.Println("Pas compris")
-			CheckWin()
-		}
-	}
-}
-
-func CheckLoose() {
-	ClearTerminal()
-	if Graph >= 7 {
-		fmt.Println("Vous avez perdu!")
-		fmt.Println("Le mot était :", Word)
-		fmt.Println("Refaire une partie?")
-		fmt.Println("1: Oui")
-		fmt.Println("2: Non")
-		var input string
-		fmt.Print("Votre choix: ")
-		fmt.Scan(&input)
-		switch input {
-		case "1":
-			NewGame()
-		case "2":
-			fmt.Println("Exiting...")
-			os.Exit(0)
-		default:
-			fmt.Println("Pas compris")
-			CheckLoose()
-		}
-	} else {
-		Display()
-	}
-}
-
-func IsInGuessed(w string) bool {
-	for _, v := range Guessed {
-		if v == w {
+// Check si la lettre est dans le mot
+// Params: letter = lettre en input
+func IsInWord(letter string) bool {
+	for _, v := range Word {
+		if strings.EqualFold(letter, string(v)) {
 			return true
 		}
 	}
 	return false
+}
+
+// Check si le guess est le mot
+// Params: word = mot en input
+func GuessWord(word string) {
+	if !IsWordInGuessed(word) {
+		WordGuessedList = append(WordGuessedList, word)
+		if strings.EqualFold(word, Word) {
+			win = true
+			Win()
+		} else {
+			ClearTerminal()
+			fmt.Println("Mauvaise réponse !")
+			fmt.Println("")
+			fmt.Println("")
+			Graph += 2
+			CheckLoose()
+		}
+	} else {
+		ClearTerminal()
+		fmt.Println("Vous avez déjà essayé ce mot!")
+		fmt.Println("")
+		fmt.Println("")
+		Display()
+	}
+}
+
+// Modifie le tableau d'underscore en rajoutant la lettre trouvées
+// Params: guess = lettre en input
+func ChangeTableau(guess string) {
+	for i, v := range Word {
+		if string(v+32) == guess || string(v-32) == guess || string(v) == guess {
+			TabUnder[i] = string(v)
+		}
+	}
+}
+
+// Vérifie si la game est gagné
+func CheckWin() bool {
+	if slices.Contains(TabUnder, "_") {
+		return false
+	} else {
+		win = true
+		Win()
+		return true
+	}
+}
+
+// Affichage di win et possibilité de relancer une partie
+func Win() {
+	if win {
+		ClearTerminal()
+		fmt.Println("Vous avez gagné!")
+		fmt.Println("Le mot était :", Word)
+		fmt.Println(("-------------"))
+		fmt.Println("")
+		fmt.Println("Refaire une partie ?")
+		fmt.Println("")
+		fmt.Println("1: Oui")
+		fmt.Println("2: Non")
+		fmt.Println("")
+		var input string
+		fmt.Print("Votre choix: ")
+		fmt.Scan(&input)
+		switch input {
+		case "1":
+			NewGame()
+		case "2":
+			fmt.Println("Exiting...")
+			os.Exit(0)
+		default:
+			fmt.Println("Pas compris")
+			CheckWin()
+		}
+	}
+}
+
+// Vérifie si la game est perdue
+func CheckLoose() {
+	if Graph >= 10 {
+		ClearTerminal()
+		DisplayHangman()
+		fmt.Println("Vous avez perdu!")
+		fmt.Println("")
+		fmt.Println("Le mot était :", Word)
+		fmt.Println("")
+		fmt.Println("")
+		fmt.Println("Refaire une partie?")
+		fmt.Println("")
+		fmt.Println("1: Oui")
+		fmt.Println("2: Non")
+		fmt.Println("")
+		var input string
+		fmt.Print("Votre choix: ")
+		fmt.Scan(&input)
+		switch input {
+		case "1":
+			NewGame()
+		case "2":
+			ClearTerminal()
+			fmt.Println("Exiting...")
+			os.Exit(0)
+		default:
+			fmt.Println("Pas compris")
+			CheckLoose()
+		}
+	} else {
+		Display()
+	}
+}
+
+// Vérifie si le mot est déjà dans la liste des mots déjà rentrés
+// Params: w = mot en input
+func IsWordInGuessed(w string) bool {
+	for _, v := range WordGuessedList {
+		if strings.EqualFold(v, w) {
+			return true
+		}
+	}
+	return false
+}
+
+// Vérifie si la lettre est déjà dans la liste des lettres déjà rentrés
+// Params: l = lettre en input
+func IsLetterInGuessed(l string) bool {
+	for _, v := range LetterGuessedList {
+		if strings.EqualFold(v, l) {
+			return true
+		}
+	}
+	return false
+}
+
+// Affichage du pendu
+func DisplayHangman() {
+	if Graph == 0 {
+		Graph = 1
+	}
+	if Graph >= 11 {
+		Graph = 10
+	}
+	file, err := os.Open("src/GraphHangman/hangman.txt")
+	if err != nil {
+		log.Fatalf("Error: %s", err)
+	}
+	defer file.Close()
+	var lines []string
+	for i := 0; i <= 9; i++ {
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			lines = append(lines, scanner.Text())
+		}
+	}
+	set := Graph * 10
+	for i := 10 * (Graph - 1); i < set; i++ {
+		fmt.Println(lines[i])
+	}
+}
+
+// Définie le nombre de lettres bonus en fonction de la taille du mot
+func NbrRandom() {
+	bonusLetters := len(Word) / 5
+	RandomLetters(bonusLetters)
+}
+
+// Définis aléatoirement les lettres bonus, en évitant les doublons et les caractère spéciaux données d'office
+// Params: nbr = nombre de lettres
+func RandomLetters(nbr int) {
+	for i := 0; i < nbr; i++ {
+		id := rand.Intn(len(Word))
+		if IsLetterInGuessed(string(Word[id])) {
+			i--
+			continue
+		}
+		if string(Word[id]) == "-" {
+			i--
+			continue
+		}
+		ChangeTableau(string(Word[id]))
+		LetterGuessedList = append(LetterGuessedList, string(Word[id]))
+	}
+
+}
+
+// Ouvre un fichier .txt et choisis aléatoirement un mot à l'intérieur
+// Params: path = chemin du fichier .txt
+func ShowTextFromFile(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Une erreur s'est produite lors de la recherche du fichier")
+		fmt.Println("")
+		fmt.Println("")
+		log.Fatal(err)
+	}
+	defer file.Close()
+	sc := bufio.NewScanner(file)
+	lines := make([]string, 0)
+	for sc.Scan() {
+		lines = append(lines, sc.Text())
+	}
+	if err := sc.Err(); err != nil {
+		log.Fatal(err)
+	}
+	randomIndex := rand.Intn(len(lines))
+	Word = lines[randomIndex]
+	Underscore(Word)
+	NbrRandom()
+	Display()
 }
